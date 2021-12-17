a3e1accbba7d26d09b05339f782394bbad009690
Talha Yazar <talhayazarg@gmail.com>
2021-12-17 16:57:45 +0300

    Exception handling added

---
+++ Patika-Dev-Projects/C#-101/Practices/Exception-Handling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     Console.WriteLine("Write some number");
+            //     int num = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine($"Number you've passed is = {num}");
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine($"Hata : {ex.Message.ToString()}");
+            // }
+            // finally
+            // {
+            //     System.Console.WriteLine("Process Completed!");
+            // }
+
+            try
+            {
+                // int a = int.Parse(null);
+                // int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("you entered empty value");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                System.Console.WriteLine("Data type is not matching");
+                System.Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                System.Console.WriteLine("Number you entered is too low or too high.");
+                System.Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("proces successfully completed.");
+            }
+        }
+    }
+}
