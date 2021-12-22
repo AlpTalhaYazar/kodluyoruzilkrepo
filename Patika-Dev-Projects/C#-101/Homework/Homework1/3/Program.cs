d58f8ca8ae343a563ddb9049d19c243078d4cc9a
Talha Yazar <talhayazarg@gmail.com>
2021-12-22 14:56:11 +0300

    added Homework1 3. console application

---
+++ Patika-Dev-Projects/C#-101/Homework/Homework1/3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello please enter a positive integer");
+            int positiveInteger1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Please enter {positiveInteger1} amount word(press Enter after each integer to submit)");
+            string[] words1 = new string[positiveInteger1];
+            for (int i = 0; i < positiveInteger1; i++)
+            {
+                words1[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(words1);
+
+            foreach (var word1 in words1)
+                Console.WriteLine(word1);
+        }
+    }
+}
