f88de8e4cb4a37b993f8c7adc0588c5be1ea0641
Talha Yazar <talhayazarg@gmail.com>
2021-12-22 14:55:15 +0300

    added Homework1 1. console application

---
+++ Patika-Dev-Projects/C#-101/Homework/Homework1/1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello please enter a positive integer");
+            int positiveInteger1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Please enter {positiveInteger1} amount positive integer(press Enter after each integer to submit)");
+            int[] nums1 = new int[positiveInteger1];
+            for (int i = 0; i < positiveInteger1; i++)
+            {
+                nums1[i] = int.Parse(Console.ReadLine());
+            }
+            
+            foreach (var num1 in nums1.FindEvens())
+                Console.WriteLine(num1);
+        }
+    }
+
+    public static class Extension
+    {
+        public static int[] FindEvens(this int[] input)
+        {
+            input = input.Where(x => x % 2 == 0).ToArray();
+            return input;
+        }
+    }
+}
