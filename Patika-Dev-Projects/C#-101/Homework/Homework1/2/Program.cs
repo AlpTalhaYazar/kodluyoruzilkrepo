2b4a21c446880fc9cb76600cced1de568ed640e0
Talha Yazar <talhayazarg@gmail.com>
2021-12-22 14:55:56 +0300

    added Homework1 2. console application

---
+++ Patika-Dev-Projects/C#-101/Homework/Homework1/2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello please enter 2 positive integer");
+            int positiveInteger1 = int.Parse(Console.ReadLine());
+            int positiveInteger2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Please enter {positiveInteger1} amount positive integer(press Enter after each integer to submit)");
+            int[] nums1 = new int[positiveInteger1];
+            for (int i = 0; i < positiveInteger1; i++)
+            {
+                nums1[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var num1 in FindEqualAndFactors(positiveInteger2, nums1))
+                Console.WriteLine(num1);
+        }
+
+        public static int[] FindEqualAndFactors(int inputInteger, int[] inputArray)
+        {
+            inputArray = inputArray.Where(x => x % inputInteger == 0).ToArray();
+            return inputArray;
+        }
+    }
+}
