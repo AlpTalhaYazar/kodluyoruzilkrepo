eed78aca9e182eb9de27f5d843b2a1011ee7740f
Talha Yazar <talhayazarg@gmail.com>
2021-12-22 14:56:43 +0300

    added Homework1 4. console application

---
+++ Patika-Dev-Projects/C#-101/Homework/Homework1/4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence to below...");
+            string sentence1 = Console.ReadLine();
+
+            string[] splittedSentence1 = sentence1.Split(" ");
+
+            int wordAmount = splittedSentence1.Length;
+            int letterAmount = string.Join("", splittedSentence1).Length;
+
+            System.Console.WriteLine($"Total word amount inside your sentence is : {wordAmount}\nTotal letter amount inside your sentence is : {letterAmount}(without white spaces)");
+        }
+    }
+}
